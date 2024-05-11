de195f09b2721bf5a9f7bfc84cb41f10f5e0bc67
Kiril Lazarov <kirillazarov@abv.bg>
2024-05-11 20:06:27 +0300

    Change strategy: new functions are added and some of the old functions are renamed

--- Calculator/common_calculator/staticfiles/JavaScripts/functions.js
+++ Calculator/common_calculator/staticfiles/JavaScripts/functions.js
@@ -1,20 +1,3 @@
-// A collection of functions
-// that take the values input from the calculator's display,
-// process them, and output them to the result screen.
-
-
-let digitsMassive = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-let operationsMassive = ['+','-','*','/']
-
-//Extracts the given pattern from input screen
-function extractStringPattern(stringValue,regexPattern) {
-    const matches = [...stringValue.matchAll(regexPattern)];
-
-
-    //Return massive with all matched patterns
-    return matches.map(match => match[0])
-}
-//Allows only digits, mathematical operations, and a dot to be entered from the keyboard.
 function isNumberKey(evt) {
     let charCode = (evt.which) ? evt.which : evt.keyCode;
     return (charCode >= 48 && charCode <= 57) || // digits from 0 - 9
@@ -30,132 +13,172 @@ function isNumberKey(evt) {
 
 }
 
-function clearScreen(value, screen, resultScreen, redirectToHandleInput = true) {
+//Extracts the given pattern from input screen
+function extractStringPattern(stringValue, regexPattern) {
+    const matches = [...stringValue.matchAll(regexPattern)];
+
+    //Return massive with all matched patterns
+    return matches.map(match => match[0]);
+}
+
+function clearScreen(value, screen) {
+    let newScreenValue = screen.value
     if (value === 'C') {
-        screen.value = '';
-        resultScreen.value = '';
+        newScreenValue = '';
 
     } else if (value === 'BACK') {
-        screen.value = screen.value.slice(0, -1);
+        newScreenValue = screen.value.slice(0, -1);
 
     }
-    if (redirectToHandleInput) {
-        handleInput(screen, resultScreen);
-    }
-
-
-}
-// This function does not allow more than one operation
-// sign on the screen, except for the combination '*-'
-function operationsInputPreprocessing(screen){
-    let inputScreenValue = screen.value.charAt(screen.value.length - 1)
-
-
-    if (screen.value.length === 1 && inputScreenValue !== '-'){
-        screen.value = '';
-    }
-
-    else if (screen.value.length === 2){
-        let previousScreenValue = screen.value.charAt(screen.value.length - 2);
-        if (previousScreenValue === '-'){
-            screen.value = previousScreenValue ;
-        }
-
-    }
-
-    else {
-        let previousScreenValue = screen.value.charAt(screen.value.length - 2)
-        if (operationsMassive.includes(inputScreenValue) &&
-            operationsMassive.includes(previousScreenValue)){
-            if (screen.value.includes('*-' + inputScreenValue)){
-                screen.value = screen.value.replace('*-' + inputScreenValue, inputScreenValue)
-            }
-            else if (previousScreenValue + inputScreenValue !== '*-'){
-                screen.value = screen.value.slice(0, -2)
-                screen.value += inputScreenValue
-            }
-
-        }
-    }
+    return newScreenValue
 }
 
+let digits = '0123456789';
+let operations = '+-*/%';
 
-// Clean the input from redundant zeroes
-function inputZeroesPreprocessing(screen){
+//Defines which character can be typed on the screen after another character
+let nextAllowedCharacter = {
+    'first': digits + '-',
+    '0': operations + '.',
+    '+': digits,
+    '-': digits,
+    '*': digits + '-',
+    '/': digits,
+    '.': digits,
+    '%': digits + operations
+}
 
-    // Find all numbers in the stringValue.
-    let regexPattern = /\d+\.\d+|\d+/gm
-    const matchedPatternsMassive = extractStringPattern(screen.value, regexPattern);
-    let lastNumber = matchedPatternsMassive[matchedPatternsMassive.length - 1]
-    if (lastNumber) {
-        let inputValue = lastNumber.charAt(lastNumber.length - 1); //
-        let preLastChar = lastNumber.charAt(lastNumber.length - 2)
-        if (lastNumber.length > 1 && preLastChar === '0') {
-            if (digitsMassive.includes(inputValue)) {
-                if (!lastNumber.includes('0.')) {
-                    screen.value = screen.value.slice(0, -1)
+// Filters inputs
+function handleInputs(screen, inputValue, elementType) {
+    let newScreenValue = screen.value
+    let lastChar = screen.value.charAt(screen.value.length - 1)
+
+    if (lastChar === '') {
+        if (nextAllowedCharacter["first"].includes(inputValue)) {
+            newScreenValue += inputValue;
+            return newScreenValue
+        }
+    } else {
+        if (elementType === 'clear_function') {
+            return clearScreen(inputValue, screen)
+        } else if (Object.keys(nextAllowedCharacter).includes(lastChar)) {
+            let regexPattern = /\d+\.\d+|\d+/gm;
+            const matchedPatternsMassive = extractStringPattern(screen.value, regexPattern);
+            let lastNumber = matchedPatternsMassive[matchedPatternsMassive.length - 1];
+
+
+            if (lastNumber.length === 1 && nextAllowedCharacter[lastChar].includes(inputValue)) {
+                newScreenValue += inputValue;
+                return newScreenValue
+            } else if (lastNumber.length > 1) {
+                if (operations.includes(inputValue) && nextAllowedCharacter[lastChar].includes(inputValue)) {
+                    newScreenValue += inputValue;
+                    return newScreenValue
+                } else if (digits.includes(inputValue)) {
+                    newScreenValue += inputValue;
+                    return newScreenValue
                 }
 
             }
+        } else {
+            newScreenValue += inputValue;
+            return newScreenValue
         }
+
     }
+    return newScreenValue
 }
 
-// Decides which of the input preprocessing functions to call.
-function inputPreprocessingDispatcher(screen) {
-    let inputValue = screen.value.charAt(screen.value.length - 1)
-    if (operationsMassive.includes(inputValue)){
-        operationsInputPreprocessing(screen)
+// eval() cannot compute percentages.
+// This function replaces each `%` symbol with *1/100
+// and rewrites the input where necessary in a way suitable for eval().
+function transformInputWithPercentageSymbols(screenValue) {
+    if (!screenValue.includes('%')) {
+        return screenValue
     }
-    else if (digitsMassive.includes(inputValue)){
-        inputZeroesPreprocessing(screen)
-    }
+    let regexPattern = /[+*-\/]?\d*\.?\d*%*/gm
+
+    const matchedPatternsMassive = extractStringPattern(screenValue, regexPattern);
+
+    let evalString = ''
+    let onlyMultiplyCase = false
+
+
+    matchedPatternsMassive.forEach(el => {
+
+        if (!el.includes('%')) {
+            if (el.toString() !=='*'){
+                evalString += el;
+            }
+            else {
+                onlyMultiplyCase = true
+            }
+        } else {
+
+            if (el.includes('*') || (el.includes('/'))) {
+                el = el.replace(/%/g, '*1/100')
+                evalString += el;
+
+            } else if (el.includes('+') || el.includes('-')) {
+                el = el.replace(/%/g, '*1/100')
+                if (onlyMultiplyCase){
+                    el = '*' + el;
+                    onlyMultiplyCase = false
+                }
+                evalString = '(' + evalString + ')' + '*(1' + el + ')'
+            } else {
+                el = el.replace(/%/g, '*1/100')
+                evalString += el;
+            }
+        }
+
+    })
+    return evalString
 }
 
-function handleInput(screen, resultScreen) {
 
+function calculateOutput(screen) {
+    let newScreenValue = screen.value;
+    newScreenValue = transformInputWithPercentageSymbols(newScreenValue)
+    let resultScreen = '';
     try {
-        resultScreen.value = eval(screen.value);
+        resultScreen = eval(newScreenValue);
 
-        if (resultScreen.value === 'undefined') {
-            resultScreen.value = '';
-        } else if (resultScreen.value === 'Infinity' ||
-            resultScreen.value === '-Infinity' ||
-            resultScreen.value === 'NaN') {
+        if (resultScreen === undefined) {
+            resultScreen = '';
+        } else if (resultScreen === Infinity ||
+            resultScreen === -Infinity ||
+            resultScreen.toString() === 'NaN') {
 
-            resultScreen.value = 'ERROR!\nDivision by 0';
+            resultScreen = 'ERROR!\nDivision by 0';
         }
     } catch (error) {
         let inputValue = screen.value.charAt(screen.value.length - 1)
         if (inputValue === '.') {
-            clearScreen('BACK', screen, resultScreen);
+            screen.value = clearScreen('BACK', screen);
+
+            // Recursive call. There is no danger of infinite invocation,
+            // as the previous input value is passed here.
+            resultScreen = calculateOutput(screen);
         } else if (inputValue === '=') {
-            clearScreen('BACK', screen, resultScreen, false);
+            screen.value = clearScreen('BACK', screen);
+            resultScreen = calculateOutput(screen);
             screen.value = '';
         }
-    }
-}
 
+    }
+    return resultScreen
+}
 
 function clickButton(element) {
     // Get needed DOM elements
     let screen = document.getElementById("screen");
     let resultScreen = document.getElementById('resultScreen')
     let elementType = element.getAttribute('class');
-    let elementValue = element.value;
+    let inputValue = element.value;
 
-    if (elementType === 'clear_function') {
-        clearScreen(elementValue, screen, resultScreen)
-    } else {
-        screen.value += elementValue
-
-        inputPreprocessingDispatcher(screen)
-        handleInput(screen, resultScreen)
-    }
+    screen.value = handleInputs(screen, inputValue, elementType);
+    resultScreen.value = calculateOutput(screen)
 
 }
-
-
-
-
 
